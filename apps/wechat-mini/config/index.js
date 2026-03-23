57a4087af0115fbff8e590255a8c90dcc7813f84
babytech <babytech@126.com>
2026-03-24 01:58:57 +0800

    chore: update mini-program apiBaseUrl to cloud hosting endpoint

--- apps/wechat-mini/config/index.js
+++ apps/wechat-mini/config/index.js
@@ -1,6 +1,6 @@
 module.exports = {
   // Replace with your deployed backend URL.
-  apiBaseUrl: "http://127.0.0.1:9000",
+  apiBaseUrl: "https://emotion-culture-api-237560-9-1415063583.sh.run.tcloudbase.com",
 
   // Replace with your WeChat cloud environment id.
   cloudEnv: "emotion-culture-cloud-dev-952b26",
